d5d2818abe90c424254573eaecf101ea953a8a4b
SirMetathyst <SirMetathyst@outlook.com>
2017-12-29 21:06:12 +0000

    Update

--- Clean.sh
+++ Clean.sh
@@ -3,5 +3,20 @@ rm -rf ./Psythyst.Core.Cli/bin
 rm -rf ./Psythyst.Core.Cli/obj
 rm -rf ./Psythyst.Core.Cli/Publish
 
+# Clean Psythyst
+rm -rf ./Psythyst.Core/Psythyst/Psythyst/bin
+rm -rf ./Psythyst.Core/Psythyst/Psythyst/obj
+rm -rf ./Psythyst.Core/Psythyst/Psythyst/Publish
+
+# Clean Psythyst.Core
+rm -rf ./Psythyst.Core/Psythyst.Core/bin
+rm -rf ./Psythyst.Core/Psythyst.Core/obj
+rm -rf ./Psythyst.Core/Psythyst.Core/Publish
+
+# Clean Psythyst.Core.Data
+rm -rf ./Psythyst.Core.Data/Psythyst.Core.Data/bin
+rm -rf ./Psythyst.Core.Data/Psythyst.Core.Data/obj
+rm -rf ./Psythyst.Core.Data/Psythyst.Core.Data/Publish
+
 # Clean
 rm -rf ./Publish
